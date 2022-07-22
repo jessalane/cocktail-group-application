822c8ab19356e79079b26d39ff55dcad5ba20e9c
Jessica Lane <jessica.lane.a@gmail.com>
2022-07-21 21:41:08 -0600

    group coding in live share

--- assets/js/cocktail.js
+++ assets/js/cocktail.js
@@ -1,36 +1,70 @@
-var repoList = document.querySelector('');
-var searchByName = document.getElementById('');
+var searchByName = $('#search-button');
 // Update ID to our running list
-var cocktailList = document.querySelector ('#cocktail-list');
-cocktailList.appendChild (testCocktailCard);
+var recipe = $('#recipe');
+// cocktailList.appendChild (testCocktailCard);
 
 function getApi() {
-  var searchCocktail = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+  var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
+  .then(function (data) {
+    console.log(data);
+
+
+    for (var i = 0; i < data.drinks.length; i++) {
+
+      var drinkName = data.drinks[i].strDrink;
+      var drinkImg = data.drinks[i].strDrinkThumb;
+      var id = data.drinks[i].idDrink;
+      var instructions = data.drinks[i].strInstructions + i;
+      
+      console.log(id);
+    
+      // var cocktailCard = function (drinkName, drinkImg, id, instructions)
+      // recipe.append (cocktailCard);
+    }
+  });
+
+
 }
 
+getApi();
 
-function createCocktailCard (name, imageUrl, id) {
-  var result = document.createElement ('div');
 
-  // Setting the class
-  result.classList.add ('card');
+// searchByName.click(getApi());
+
+// function createCocktailCard (name, imageUrl, id) {
+//   var result = document.createElement ('div');
+
+//   // Setting the class
+//   result.classList.add ('card');
   
-  var h3 = document.createElement ('h3');
-  h3.textContent = name;
-  result.appendChild (h3);
+//   var h3 = document.createElement ('h3');
+//   h3.textContent = name;
+//   result.appendChild (h3);
 
-  var img = document.createElement ('img');
-  img.src = imageUrl;
-  result.appendChild (img);
+//   var img = document.createElement ('img');
+//   img.src = imageUrl;
+//   result.appendChild (img);
 
-  return result;
-}
-// Add one cocktail to the body element by calling the function with some arguments
-var testCocktailCard = createCocktailCard ("Stinger", "https:\/\/www.thecocktaildb.com\/images\/media\/drink\/2ahv791504352433.jpg", 17193);
+//   return result;
+// }
+// // I think we need this in the getApi function
+// for (var i = 0; i < drinks.length; i++) {
+//   var drink = drinks[i];
+//   var name = drink.strDrink;
+//   var imageUrl = drink.strDrinkThumb;
+//   var id = drink.idDrink;
 
-document.body.appendChild (testCocktailCard);
+  // var cocktailCard = createCocktailCard (name, imageUrl, id);
+  // cocktailList.appendChild (cocktailCard);
+// }
+
+// // Add one cocktail to the body element by calling the function with some arguments
+// var testCocktailCard = createCocktailCard ("Stinger", "https:\/\/www.thecocktaildb.com\/images\/media\/drink\/2ahv791504352433.jpg", 17193);
+
+// document.body.appendChild (testCocktailCard);
+
