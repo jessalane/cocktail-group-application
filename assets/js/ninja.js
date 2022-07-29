84d327065ae3452f626fc5fdbc97d77efe469027
Tjex07 <tjex07@hotmail.com>
2022-07-28 18:35:18 -0600

    no change

--- assets/js/ninja.js
+++ assets/js/ninja.js
@@ -14,3 +14,43 @@ $.ajax({
         console.error('Error: ', jqXHR.responseText);
     }
 });
+
+    // // inserting variables into recipe html
+    // $("#titleName").html(drinkName);
+    // $("#subGlass").html("use a " + drinkGlass);
+    // $("#drinkImg").children(1).attr("src", drinkImg);
+    // $("#directions").html(instructions);
+
+
+
+
+//   // checks if the tags variable has value
+//   if (tags === null) {
+//     // inserts empty string instead of null
+//     $("#drinkTags").html(" ");
+
+//   } else {
+//     // inserts tags if it has value
+//     $("#drinkTags").html(tags);
+//   }
+
+//   // function rotating through populated ingredients
+//   function pushIngredient() {
+//     // emptying the ingredients before populating more
+//     $("#ingredients-container").empty();
+//     // rotating i though the number of available ingredients
+//     for (i = 0; i < ingredients.length; i++) {
+
+//       // checking that ingredients have value
+//       if (ingredients[i].ingredient !== null) {
+//         // checking that measure has value
+//         if (ingredients[i].measure !== null) {
+//           // only appending both when they have a string
+//           $("#ingredients-container").append(`<li> ● ${ingredients[i].measure} of ${ingredients[i].ingredient}</li>`);
+//         } else {
+//           // appends only ingredient when there is no measurement
+//           $("#ingredients-container").append(`<li> ● ${ingredients[i].ingredient}</li>`);
+//         }
+//       }
+//     }
+//   }
